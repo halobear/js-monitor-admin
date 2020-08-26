7f12884280fc27eee18a5327194636193b1cb64b
luzhongkuan <luzhongk@126.com>
2020-08-26 11:37:47 +0800

    chore: 测试环境

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -57,8 +57,8 @@ export default {
       title: process.env.VUE_APP_HOME_TITLE,
       haloPhone: process.env.VUE_APP_HALO_PHONE,
       form: {
-        username: isDev ? 'halo-error' : '',
-        password: isDev ? 'haloe0821' : '',
+        username: isDev ? 'admin' : '',
+        password: isDev ? 'admin' : '',
       },
       rules: {
         username: [
